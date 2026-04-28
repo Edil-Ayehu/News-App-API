8156471339131e65ed403b2e8ed198cba009099c
edilayehu <edilayehu@eaglelionsystems.com>
2026-04-28 15:56:56 +0300

    login endpoint and token issue fixed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,20 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        // signOptions: {
+        //   expiresIn: configService.get<string>('JWT_EXPIRES_IN')!,
+        // },
+      })
     })
   ],
   controllers: [AuthController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { RegisterDto } from './dtos/register.dto';
 import * as bcrypt from 'bcrypt'
+import { LoginDto } from './dtos/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -37,5 +38,33 @@ export class AuthService {
         }
     }
 
-    async login() {}
+    async login(loginDto: LoginDto) {
+        const user = await this.userRepo.findOne({where: {email: loginDto.email}});
+
+        if(!user) throw new UnauthorizedException("Invalid login credentials")
+
+        const isMatch = await bcrypt.compare(loginDto.password, user.password)
+
+        if(!isMatch) throw new UnauthorizedException("Invalid login credentials")
+
+        const accessToken = this.jwtService.sign(
+            {sub: user.id, email: user.email},
+            {expiresIn: '1d'}
+        );
+
+        const refreshToken = this.jwtService.sign(
+            {sub: user.id},
+            {expiresIn: '7d'}
+        );
+
+        user.refreshToken = refreshToken;
+
+        await this.userRepo.save(user)
+
+        return {
+            accessToken,
+            refreshToken,
+            user,
+        }
+    }
 }
