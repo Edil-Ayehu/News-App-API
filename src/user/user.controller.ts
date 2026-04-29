a935c68ab8cfd12e46bcb8d339f7e6542eeb1ff0
edilayehu <edilayehu@eaglelionsystems.com>
2026-04-29 16:41:13 +0300

    update profile endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { GetUser } from './decorators/get-user.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UpdateProfileDto } from './dtos/update-profile.dto';
 
 @Controller('user')
 @UseGuards(JwtAuthGuard)
@@ -13,5 +14,13 @@ export class UserController {
     @Get('profile')
     async getProfile(@GetUser() user) {
        return await this.userService.getProfile(user.sub);
+    }
+
+    @Patch('updateProfile')
+    async updateProfile(
+        @GetUser() user, 
+        @Body() dto: UpdateProfileDto,
+    ) {
+        return await this.userService.updateProfile(user.sub, dto)
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { UpdateProfileDto } from './dtos/update-profile.dto';
 
 @Injectable()
 export class UserService {
@@ -19,5 +20,11 @@ export class UserService {
         if (!user) throw new NotFoundException("User not found")
 
         return user
+    }
+
+    async updateProfile(userId: string, dto: UpdateProfileDto) {
+        await this.userRepo.update(userId, dto);
+
+        return await this.getProfile(userId);
     }
 }
